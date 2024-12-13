df4cfb8a5194eba2e2bdcc25e6ffcfa4cc6ac4b2
Danil Anikin <mart4r.clearlist@gmail.com>
2024-12-13 22:11:04 +1000

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Данил Аникин on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
